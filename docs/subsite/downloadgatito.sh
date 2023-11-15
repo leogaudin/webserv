9866c740076f8448567013a0480a80d26795b2ba
Léo <leogaudinoff@gmail.com>
2023-11-15 12:53:39 +0100

    fix: fix scripts

--- docs/subsite/downloadgatito.sh
+++ docs/subsite/downloadgatito.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo -e -n "HTTP/1.1 200 OK\r\n"
 echo -e -n "Content-Type: image/jpeg\r\n"
+echo -e -n "Content-Disposition: attachment; filename=\"elgatitohehehe.jpg\"\r\n"
 echo -e -n "\r\n"
 
 path=$(pwd)
